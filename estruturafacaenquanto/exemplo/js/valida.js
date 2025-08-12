c1e3d939b1de6eab1c0337ad09aa8a99680be442
LuanWiggers <luanwiggers.wiggers@gmail.com>
2025-08-12 17:40:04 -0300

    Adicionando pasta estruturafacaenquanto

---
+++ estruturafacaenquanto/exemplo/js/valida.js
@@ -0,0 +1,10 @@
+function valida(){
+    
+    let conta = 1;
+    do{
+
+        console.log("Passou a " +conta+ "ª vez e o valor é : " +conta);
+    }  while(conta<=10)
+
+        return false;
+}
