5be61ddf37a3a93ad4da09a9e7bd198adc5fea6e
LuanWiggers <luanwiggers.wiggers@gmail.com>
2025-08-14 18:01:49 -0300

    Finalizado desafio 2

---
+++ javascript/Desafios js/desafio 2/js/valproc.js
@@ -0,0 +1,21 @@
+function calcularIMC() {
+    console.clear();
+    let continuar = true;
+
+    while (continuar) {
+        let peso = parseFloat(prompt("Digite seu peso (em kg):"));
+        let altura = parseFloat(prompt("Digite sua altura (em metros):"));
+
+        let imc = peso / (altura * altura);
+
+        alert("Seu IMC é: " + imc.toFixed(2));
+        console.log("IMC calculado: " + imc.toFixed(2));
+
+        let resposta = prompt("Deseja calcular novamente? (s para sim / qualquer outra tecla para sair)");
+
+        if (resposta.toLowerCase() !== 's') {
+            continuar = false;
+            alert("Programa encerrado.");
+        }
+    }
+}
