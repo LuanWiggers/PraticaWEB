103d8b9f9e41f1205b4efacb4e64dd726dbc66ad
LuanWiggers <luanwiggers.wiggers@gmail.com>
2025-08-14 20:19:56 -0300

    Fazendo desafio 3

---
+++ javascript/Desafios js/desafio 3/js/valproc.js
@@ -0,0 +1,21 @@
+function mediaSalario(){
+    let continuar = false;
+    
+    
+
+    while(continuar!="finalizar"){
+        let idade = parseInt(prompt("Digite sua idade:"));
+        let genero = parseFloat(prompt("Digite o gênero:"));
+        let salario = parseInt(prompt("Digite seu salário"));
+
+        
+
+
+        if (resposta.toLowerCase() !== 'finalizar') {
+            continuar = true;
+            alert("Programa encerrado.");
+        }
+
+        
+        }
+    }
